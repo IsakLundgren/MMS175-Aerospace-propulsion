84211fbd1afc88ce937614f56c5dee699674a6f7
isak lundgren <isak.leopold@gmail.com>
2023-11-01 11:19:24 +0100

    Calculated speed of sound

--- DT1.py
+++ DT1.py
@@ -1,8 +1,11 @@
+import numpy as np
+
+
 # Given parameters
-Altitude = 35000  # [ft]
+Altitude = 35000 * 0.3048  # m
 Flight_Mach_number = 0.82
-dT_isa = 10  # [K]
-Net_thrust = 67.3  # [kN]
+dT_isa = 10  # K
+Net_thrust = 67.3 * 1e3  # N
 BPR = 11.8
 FPR = 1.55
 OPR = 47
@@ -20,3 +23,11 @@ Hot_jet_efficiency = 99.0  # %
 Shaft_mechanical_efficiency = 99.5  # %
 IPC_polytropic_efficiency = 91.0  # %
 HPC_polytropic_efficiency = 91.5  # %
+
+# Calculate speed of sound at design altitude
+sourceAlt = [10000, 11000]
+sourceSOS = [295.2, 295.1]
+Speed_of_sound = np.interp(Altitude, sourceAlt, sourceSOS)  # m/s
+
+C_a = Flight_Mach_number * Speed_of_sound
+a = 10
