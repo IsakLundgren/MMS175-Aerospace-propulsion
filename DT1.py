b31aa7fee84968b4cbef25ea441087858be605d6
isak lundgren <isak.leopold@gmail.com>
2023-11-01 10:48:24 +0100

    Added given parameters

---
+++ DT1.py
@@ -0,0 +1,22 @@
+# Given parameters
+Altitude = 35000  # [ft]
+Flight_Mach_number = 0.82
+dT_isa = 10  # [K]
+Net_thrust = 67.3  # [kN]
+BPR = 11.8
+FPR = 1.55
+OPR = 47
+HPC_pressure_ratio = 4.5
+Turbine_inlet_temperature = 1680  # K
+Intake_efficiency = 99.5  # %
+Fan_polytropic_efficiency = 92.0  # %
+Combustor_efficiency = 99.9  # %
+Combustor_pressure_loss = 3.5  # %
+HPT_polytropic_efficiency = 90.5  # %
+IPT_polytropic_efficiency = 91.0  # %
+LPT_polytropic_efficiency = 91.5  # %
+Cold_Jet_efficiency = 98.0  # %
+Hot_jet_efficiency = 99.0  # %
+Shaft_mechanical_efficiency = 99.5  # %
+IPC_polytropic_efficiency = 91.0  # %
+HPC_polytropic_efficiency = 91.5  # %
